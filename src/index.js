018e87371ac9a285ea0bbaa29d0ed76cb88143ad
Luzinda Douglas <douglas.luzinda.ot@gmail.com>
2022-11-05 15:15:13 +0300

    Add and delete functionality

--- src/index.js
+++ src/index.js
@@ -1,55 +1,51 @@
 import _ from 'lodash';
 import './style.css';
 import printMe from './print.js';
-const data=[
-    {
-       description:"Todo listings",
-       "completed":true,
-       "index":1
-    },
-    {
-        description:"Todo listings",
-        "completed":true,
-        "index":1
-     },
-     {
-        description:"Todo listings",
-        "completed":true,
-        "index":1
-     },
-]
+window.onload=()=>{
+let cleandata=[];
 const ul=document.createElement('ul')
-const div=document.createElement('div')
-div.innerHTML=`<h3>Today's todo</h3>`
-ul.appendChild(div)
-const input=document.createElement('div')
-input.innerHTML=`<input type="text" class="enter" placeholder="Enter something" />`
-ul.appendChild(input)
+
+class todo {
+    constructor(index, description, completed) {
+      this.index = index;
+      this.description = description;
+      this.completed = completed;
+    }
+  }
+
 
 const addliststodom=()=>{ 
-
-data.map((item)=>{
-   const li=document.createElement('li')
-   li.innerHTML=`
+ul.innerHTML = ""
+cleandata=JSON.parse(localStorage.getItem('deletetodo')) || []
+cleandata.forEach((item)=>{
+   ul.innerHTML +=`
    <div class="delete">
    <div class="flexer"><input type="checkbox" class="check"/><h5>${item.description}</h5></div>
-   <h6>del</h6>
+   <h6 class="deletetodo" index=${item.index} type="button">del</h6>
    </div>
    `
-
-ul.append(li)
 })
-const item=document.querySelector(".container")
+
+const item=document.querySelector(".htmls")
 item.appendChild(ul)
 }
-
 addliststodom()
-const divx=document.createElement('div')
-divx.innerHTML=`<button>Clear all Completed</button>`
-ul.appendChild(divx)
-//  function component() {
-//    const element = document.createElement('div');
-//    return element;
-//  }
 
-//  document.body.appendChild(component());
+const addtodo=document.querySelector(".enter")
+addtodo.addEventListener("keypress",(event)=>{
+    // let lenghts=deleteitem.length
+    if (event.key === "Enter") {
+       let data=JSON.parse(localStorage.getItem("deletetodo")) || []
+       let cleandata=data
+       let todos=new todo(1,"hello world",false)
+       cleandata.push(todos)
+       console.log(...cleandata)
+       localStorage.setItem("deletetodo",JSON.stringify(cleandata))
+       ul.innerHTML = ""
+       addliststodom()
+
+    }
+})
+
+
+}
