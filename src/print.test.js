71b1fbdbb3a203f967829515b0f6349912dd90cf
Luzinda Douglas <douglas.luzinda.ot@gmail.com>
2022-11-18 19:02:49 +0300

    local storage mock

--- src/print.test.js
+++ src/print.test.js
@@ -1,17 +1,18 @@
-const {sum} = require('./print.js');
+
+const {sum,addTask} = require('./print.js');
 describe('Add new todo task', () => {
 
 test("properly adds objects to local storage",()=>{
-// localStorage.clear();
-// const obj = {
-//     description: 'new todo item',
-//     index: 1,
-//     completed: false,
-//   };
-//   Utility.addTask(obj,[])
-//   const result = JSON.parse(window.localStorage.getItem('deletetodo'));
+localStorage.clear();
+const obj = {
+    description: 'new todo item',
+    index: 1,
+    completed: false,
+  };
+  Utility.addTask(obj,[])
+  const result = JSON.parse(window.localStorage.getItem('deletetodo'));
 
-  expect(sum(2,3)).toBe(5)
+  expect(result.length).toBe(1)
 })
 
 })
